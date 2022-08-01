df53203fc6293840b9a7f220a2ee6e3cde97d3f3
Your Name <you@example.com>
2022-08-01 23:45:52 +0800

    git 注解

--- git.ps1
+++ git.ps1
@@ -1,9 +1,9 @@
-git status  # 
+git status  # 顯示 git 的狀態
 
-git diff  # 
+git diff  # git 的差異
 
-git add --all  # 
+git add --all  # 添加新的文件
 
-git commit -am 'message'  # 
+git commit -am 'message'  # 提交訊息
 
-git push  # 
+git push  # 提交
