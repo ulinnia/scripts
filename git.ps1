5a377963c12f38dfe1d258310fa4eef7557fad7f
vonvon <97818495+ulinnia@users.noreply.github.com>
2023-12-10 23:21:40 +0800

    Update git.ps1

--- git.ps1
+++ git.ps1
@@ -2,8 +2,10 @@ git status  # 顯示 git 的狀態
 
 git diff  # git 的差異
 
-git add --all  # 添加新的文件
+git add --all(-a)  # 添加新的文件
 
 git commit -am 'message'  # 提交訊息
 
 git push  # 提交
+
+git pull --rebase  #renew local file
